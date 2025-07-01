285375a126f14f07470ca9149d88c2c1680e8e13
Aleksey Lopez <lopezag@umich.edu>
2025-07-01 19:28:01 -0400

    Added comments.

--- include/nebula/PGNExporter.hpp
+++ include/nebula/PGNExporter.hpp
@@ -24,10 +24,13 @@ private:
     std::vector<std::string> moves;
     Board* board;
 
+    // gets algebraic notation for square
     inline std::string square(int sq) { return { static_cast<char>('a' + (sq % 8)), static_cast<char>('1' + (sq / 8)) }; }
 
+    // makes a move and turns it into algebraic notation
     std::string to_san(const Move& m);
 
+    // gets today's date
     std::string date();
 };
 
