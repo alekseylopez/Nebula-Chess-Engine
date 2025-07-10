39cc535d79cddd8a6466bf5074127990a144b4d0
Aleksey Lopez <lopezag@umich.edu>
2025-07-10 16:48:32 -0400

    Preparing to use ML to tune static evaluation

---
+++ tuning/scripts/tune_evaluation.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import json
+import chess
+import chess.engine
+from dataclasses import dataclass
+from typing import List, Dict, Tuple
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
