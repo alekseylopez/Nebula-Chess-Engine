b270960a3ab5c94e6f91b0a08ca90ff6b1444ac4
Aleksey Lopez <lopezag@umich.edu>
2025-07-14 01:15:44 -0400

    Corrected order

--- tuning/scripts/chess_evaluation/__init__.py
+++ tuning/scripts/chess_evaluation/__init__.py
@@ -2,8 +2,8 @@
 Python scripts that use machine learning to tune Nebula's static evaluation function
 """
 
-from .evaluate import ChessEvaluator
 from .evaluate import ChessPosition
+from .evaluate import ChessEvaluator
 
 __version__ = "0.1.0"
-__all__ = ["ChessEvaluator", "ChessPosition"]
+__all__ = ["ChessPosition", "ChessEvaluator"]
