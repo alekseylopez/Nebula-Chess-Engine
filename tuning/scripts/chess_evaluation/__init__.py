4a32ce68b784042caf328a04b2c39958e7b1eafe
Aleksey Lopez <lopezag@umich.edu>
2025-07-14 01:17:49 -0400

    Updated package initializer

--- tuning/scripts/chess_evaluation/__init__.py
+++ tuning/scripts/chess_evaluation/__init__.py
@@ -2,8 +2,9 @@
 Python scripts that use machine learning to tune Nebula's static evaluation function
 """
 
+from .evaluate import EvaluationParams
 from .evaluate import ChessPosition
 from .evaluate import ChessEvaluator
 
 __version__ = "0.1.0"
-__all__ = ["ChessPosition", "ChessEvaluator"]
+__all__ = ["EvaluationParams", "ChessPosition", "ChessEvaluator"]
