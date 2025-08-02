634a106a50d6f9bb2e293d1a77aa2e84ad6f6dda
Aleksey Lopez <lopezag@umich.edu>
2025-08-01 23:37:45 -0400

    Mask generating functions

--- src/MagicBitboards.cpp
+++ src/MagicBitboards.cpp
@@ -31,4 +31,44 @@ struct MagicInit
     }
 } _magicInit;
 
+uint64_t MagicBitboards::generate_rook_mask(int sq)
+{
+    uint64_t mask = 0ULL;
+    int r = sq / 8;
+    int f = sq % 8;
+
+    // horizontal - exclude edges
+    for(int i = f + 1; i < 7; ++i)
+        mask |= 1ULL << (r * 8 + i);
+    for(int i = f - 1; i > 0; --i)
+        mask |= 1ULL << (r * 8 + i);
+
+    // vertical - exclude edges
+    for(int i = r + 1; i < 7; ++i)
+        mask |= 1ULL << (i * 8 + f);
+    for(int i = r - 1; i > 0; --i)
+        mask |= 1ULL << (i * 8 + f);
+
+    return mask;
+}
+
+uint64_t MagicBitboards::generate_bishop_mask(int sq)
+{
+    uint64_t mask = 0ULL;
+    int r = sq / 8;
+    int f = sq % 8;
+
+    // four diagonal directions - exclude edges
+    for(int i = 1; r + i < 7 && f + i < 7; ++i)
+        mask |= 1ULL << ((r + i) * 8 + (f + i));
+    for(int i = 1; r + i < 7 && f - i > 0; ++i)
+        mask |= 1ULL << ((r + i) * 8 + (f - i));
+    for(int i = 1; r - i > 0 && f + i < 7; ++i)
+        mask |= 1ULL << ((r - i) * 8 + (f + i));
+    for(int i = 1; r - i > 0 && f - i > 0; ++i)
+        mask |= 1ULL << ((r - i) * 8 + (f - i));
+
+    return mask;
+}
+
 }
